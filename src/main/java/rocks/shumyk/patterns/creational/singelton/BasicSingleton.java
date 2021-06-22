dce78e75cc6fb9dbfa4734a532ec8e0735016b4e
Shumyk <dima.shumyk@gmail.com>
2021-06-22 21:58:31 +0300

    fixed deserialization problem of singleton, used 'readResolve' method to hint JVM to return instance of already existing object instead of creating new one

--- src/main/java/rocks/shumyk/patterns/creational/singelton/BasicSingleton.java
+++ src/main/java/rocks/shumyk/patterns/creational/singelton/BasicSingleton.java
@@ -3,7 +3,9 @@ package rocks.shumyk.patterns.creational.singelton;
 import lombok.Getter;
 import lombok.Setter;
 
-public class BasicSingleton {
+import java.io.*;
+
+public class BasicSingleton implements Serializable {
 	@Getter @Setter
 	private int value = 0;
 
@@ -14,13 +16,43 @@ public class BasicSingleton {
 	public static BasicSingleton instance() {
 		return INSTANCE;
 	}
+
+	@Serial
+	protected Object readResolve() {
+		return INSTANCE;
+	}
 }
 
 class Main {
-	public static void main(String[] args) {
-		final BasicSingleton singleton = BasicSingleton.instance();
-		singleton.setValue(123);
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		// 1. reflection
+		// 2. serialization
 
-		System.out.println(singleton.getValue());
+		var singleton = BasicSingleton.instance();
+		singleton.setValue(111);
+
+		var filename = "singleton.bin";
+		saveToFile(singleton, filename);
+
+		singleton.setValue(222);
+
+		final BasicSingleton deserializedSingleton = readFromFile(filename);
+		System.out.println("equality of singletons: " + (singleton == deserializedSingleton));
+		System.out.println("initial singleton value: " + singleton.getValue());
+		System.out.println("deserialized singleton value: " + deserializedSingleton.getValue());
+	}
+
+	static void saveToFile(final BasicSingleton singleton, final String filename) throws IOException {
+		try (var fileOut = new FileOutputStream(filename);
+			 var out = new ObjectOutputStream(fileOut)) {
+			out.writeObject(singleton);
+		}
+	}
+
+	static BasicSingleton readFromFile(final String filename) throws IOException, ClassNotFoundException {
+		try (var fileIn = new FileInputStream(filename);
+			 var in = new ObjectInputStream(fileIn)) {
+			return (BasicSingleton) in.readObject();
+		}
 	}
 }
